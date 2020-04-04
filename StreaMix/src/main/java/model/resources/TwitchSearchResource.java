b91dcae733e7150068f31cb9206d024205c2c2a0
Jose Nieves <esoj.nieves@gmail.com>
2020-04-04 17:56:37 +0200

    Arreglos para el controller de twitch

--- StreaMix/src/main/java/model/resources/TwitchSearchResource.java
+++ StreaMix/src/main/java/model/resources/TwitchSearchResource.java
@@ -5,24 +5,35 @@ import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.restlet.engine.header.HeaderConstants;
+import org.restlet.Request;
+import org.restlet.data.Header;
 import org.restlet.resource.ClientResource;
+import org.restlet.util.Series;
 
 import model.twitch.TwitchSearch;
 
 
 public class TwitchSearchResource {
-	public static final String apiKey = "vmyhjn4lvbiyfe1u4rn12km8jyx6qn";
+	public static final String clientID = "vmyhjn4lvbiyfe1u4rn12km8jyx6qn";
 	public static final Logger log = Logger.getLogger(TwitchSearchResource.class.getName());
 	
 	public TwitchSearch getStreams(String query) throws UnsupportedEncodingException {
 		String queryFormatted = URLEncoder.encode(query, "UTF-8");
 		String uri = "https://api.twitch.tv/kraken/search/streams?query=" + queryFormatted;
 		
-		log.log(Level.WARNING, "twitchSearch URI: "+uri);
+		log.log(Level.INFO, "twitchSearch URI: "+uri);
 		
 		ClientResource cr = new ClientResource(uri);
-		TwitchSearch twS = cr.get(TwitchSearch.class);
+		Request req = cr.getRequest();
 		
+		Series<Header> headerValue = new Series<Header>(Header.class);
+	    req.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS, headerValue);
+	    headerValue.add("Client-ID", clientID);
+	    
+	    log.log(Level.INFO, "Header añadido");
+	    
+	    TwitchSearch twS = cr.get(TwitchSearch.class);
 		return twS;
 	}
 }
